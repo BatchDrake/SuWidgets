c1ddad408a964601758fc1162fc51bde272a0380
Ing. Jaroslav Safka <devel@safka.org>
2021-07-02 09:21:52 +0200

    Add default values

--- BookmarkInfo.h
+++ BookmarkInfo.h
@@ -7,10 +7,10 @@
 
 struct BookmarkInfo {
   QString name; ///< name of bookmark
-  qint64 frequency; ///< [Hz] center frequency
+  qint64 frequency = 0; ///< [Hz] center frequency
   QColor color; ///< color of bookmark
-  qint32 lowFreqCut; ///< [Hz] offset from frequency (mostly negative)
-  qint32 highFreqCut; ///< [Hz] offset from frequency (moslty positive)
+  qint32 lowFreqCut = 0; ///< [Hz] offset from frequency (mostly negative)
+  qint32 highFreqCut = 0; ///< [Hz] offset from frequency (moslty positive)
   QString modulation; ///< modulation like "AM", "FM", "USB", "LSB"
 
   qint32 bandwidth() { return highFreqCut - lowFreqCut; }
