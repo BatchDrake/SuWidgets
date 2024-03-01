BatchDrake/SuWidgets

commit 7d84060a4e6022ddb9fc971b92061318e8b21101

AbstractWaterfall: limit frequency zoom

AbstractWaterfall.cpp
@@ -718,8 +718,8 @@ void AbstractWaterfall::mouseReleaseEvent(QMouseEvent * event)
 // Make a single zoom step on the X axis.
 void AbstractWaterfall::zoomStepX(float step, int x)
 {
-  // calculate new range shown on FFT
-  qint64 new_range = qBound(10.0f,
+  // calculate new range shown on FFT, at least 5 bins
+  qint64 new_range = qBound(5.0f * m_SampleFreq / m_fftDataSize,
       (float)(m_Span) * step,
       (float)(m_SampleFreq) * 10.0f);
 

commit ebe506e50ce090f2bad07b893e485c7094d86bd6

AbstractWaterfall: avoid division by zero on zoom
Fixes a regression introduced in 7d84060a4e6022ddb9fc971b92061318e8b21101
where a waterfall without any FFT data goes berserk on zoom.

AbstractWaterfall.cpp
@@ -719,7 +719,7 @@ void AbstractWaterfall::mouseReleaseEvent(QMouseEvent * event)
 void AbstractWaterfall::zoomStepX(float step, int x)
 {
   // calculate new range shown on FFT, at least 5 bins
-  qint64 new_range = qBound(5.0 * m_SampleFreq / m_fftDataSize,
+  qint64 new_range = qBound(m_fftDataSize > 0 ? 5.0 * m_SampleFreq / m_fftDataSize : 10.0,
       (double)(m_Span) * step,
       (double)(m_SampleFreq) * 10.0);
 